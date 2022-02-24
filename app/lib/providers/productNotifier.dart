6a667637496840c7a0df9a74ec5273a4264db189
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-02-24 11:00:54 +0530

    privateAttributes created

--- app/lib/providers/productNotifier.dart
+++ app/lib/providers/productNotifier.dart
@@ -7,43 +7,45 @@ import 'package:pm_app/models/productModel.dart';
 import 'package:http/http.dart' as http;
 
 class ProductNotifier extends ChangeNotifier {
-  ProductModel productModel = ProductModel(
-    data: Data(
-      id: 0,
-      attributes: Attributes(
-        title: '',
-        price: 0,
-        description: '',
-        qty: '',
-      ),
-    ),
-  );
+  // ProductModel productModel = ProductModel(
+  //   data: Data(
+  //     id: 0,
+  //     attributes: Attributes(
+  //       title: '',
+  //       price: 0,
+  //       description: '',
+  //       qty: '',
+  //     ),
+  //   ),
+  // );
+
+  // ProductModel get getProduct => productModel;
+
+  // Future<ProductModel> getProductModel() async {
+  //   final url = Uri.parse("http://10.0.2.2:1337/api/products/1");
+
+  //   try {
+  //     final response = await http.get(url);
+  //     log(response.statusCode.toString());
+
+  //     if (response.statusCode == 200) {
+  //       productModel = ProductModel.fromJson(json.decode(response.body));
+  //       notifyListeners();
+  //     } else {
+  //       throw Exception("API Error");
+  //     }
+  //   } catch (e) {
+  //     print(e.toString());
+  //   }
+  //   return productModel;
+  // }
 
   ProductListModel _productListModel = ProductListModel(
     data: [],
   );
 
-  ProductModel get getProduct => productModel;
+  
   ProductListModel get getProductList => _productListModel;
-
-  Future<ProductModel> getProductModel() async {
-    final url = Uri.parse("http://10.0.2.2:1337/api/products/1");
-
-    try {
-      final response = await http.get(url);
-      log(response.statusCode.toString());
-
-      if (response.statusCode == 200) {
-        productModel = ProductModel.fromJson(json.decode(response.body));
-        notifyListeners();
-      } else {
-        throw Exception("API Error");
-      }
-    } catch (e) {
-      print(e.toString());
-    }
-    return productModel;
-  }
 
   Future<void> getProductListModel() async {
     final url = Uri.parse("http://10.0.2.2:1337/api/multi-products");
